e5bae3fe061ca1dc3852a359176785f22820327a
QHV8126 <andcarmay@alum.us.es>
2024-11-19 12:30:17 +0100

    ejercicios clase 19 nov

--- consultas.sql
+++ consultas.sql
@@ -70,3 +70,94 @@ FROM Clientes
     JOIN Pedidos ON Clientes.id = Pedidos.clienteId
     JOIN Usuarios ON Usuarios.id = Clientes.usuarioId
 WHERE TIMESTAMPDIFF(YEAR, Clientes.fechaNacimiento, Pedidos.fechaRealizacion)>=18;
+
+
+-- inventados profesor
+
+-- productos precio más caro y más barato
+SELECT *
+FROM Productos
+WHERE
+    precio = (SELECT MAX(Productos.precio) FROM Productos)
+    OR
+    precio = (SELECT MIN(Productos.precio) FROM Productos);
+
+-- nombre producto, precio +10% de los <50€
+SELECT Productos.nombre, 1.1*Productos.precio
+FROM Productos
+WHERE precio < 50.00;
+
+-- nombre producto, precio +10% de los <50€ (nombres bonitos, precio rondeado)
+SELECT Productos.nombre AS Producto, ROUND(1.1*Productos.precio, 2) AS Precio
+FROM Productos
+WHERE precio < 50.00;
+
+-- nombre producto, precio +10% de los <50€ y los otros sin modificar
+SELECT Productos.nombre AS Producto, ROUND(1.1*Productos.precio, 2) AS Precio
+FROM Productos
+WHERE precio < 50.00
+UNION
+SELECT Productos.nombre, 1.1*Productos.precio
+FROM Productos
+WHERE precio >= 50.00;
+
+-- nombre producto, precio +10% de los <50€ y los otros sin modificar (ORDENADO)
+SELECT Productos.nombre AS Producto, ROUND(1.1*Productos.precio, 2) AS Precio
+FROM Productos
+WHERE precio < 50.00
+UNION
+SELECT Productos.nombre, 1.1*Productos.precio
+FROM Productos
+WHERE precio >= 50.00
+ORDER BY precio DESC -- o ASC;
+
+-- clientes sin pedidos
+SELECT Usuarios.nombre
+FROM Usuarios
+    JOIN Clientes ON Usuarios.id = Clientes.usuarioId
+    LEFT JOIN Pedidos ON Clientes.id = Pedidos.clienteId
+WHERE Pedidos.id IS NULL;
+
+-- clientes con pedidos con productos > 100
+SELECT DISTINCT Clientes.id
+FROM Clientes
+    JOIN Pedidos ON Clientes.id = Pedidos.clienteId
+    JOIN LineasPedido ON LineasPedido.pedidoId = Pedidos.id
+WHERE LineasPedido.precio >= 100;
+
+-- empleados con salario > salario medio
+SELECT *
+FROM Empleados
+WHERE salario >= (SELECT AVG(Empleados.salario) FROM Empleados);
+
+-- clientes nacidos este mes (fecha parseada)
+SELECT Usuarios.nombre, Clientes.direccionEnvio, DATE_FORMAT(Clientes.fechaNacimiento, "%d-%m-%Y")
+FROM Usuarios
+    JOIN Clientes ON Clientes.usuarioId = Usuarios.id
+WHERE MONTH(Clientes.fechaNacimiento) = MONTH(CURDATE());
+
+-- cliente, importe total (orden decreciente)
+SELECT Clientes.*, COALESCE(SUM(LineasPedido.unidades * LineasPedido.precio),0) AS importe
+FROM Clientes
+    LEFT JOIN Pedidos ON Clientes.id = Pedidos.clienteId
+    LEFT JOIN LineasPedido ON LineasPedido.pedidoId = Pedidos.id
+GROUP BY Clientes.id
+ORDER BY importe DESC;
+
+-- clientes con más de 3 pedidos
+SELECT Usuarios.nombre, COUNT(Pedidos.id) AS total
+FROM Usuarios
+	JOIN Clientes ON Clientes.usuarioId = Usuarios.id
+	JOIN Pedidos ON Clientes.id = Pedidos.clienteId
+GROUP BY Clientes.id
+HAVING total >= 3; -- el group by va con HAVING no con WHERE
+
+-- unidades vendidas de cada producto
+SELECT LineasPedido.productoId, SUM(LineasPedido.unidades)
+FROM LineasPedido
+GROUP BY LineasPedido.productoId;
+
+-- produtos más populares de cada tipo
+SELECT Pedidos.productoId, SUM(LineasPedido.unidades)
+FROM Pedidos
+GROUP BY LineasPedido.productoId;
